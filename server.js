8d320834ff011c18759319ce0206bdf626464348
Stuart Osborn <stuartgosborn22@gmail.com>
2024-03-07 17:54:07 -0600

    changed port variable code

--- server.js
+++ server.js
@@ -1,30 +1,27 @@
-const express = require('express');
-const path = require('path');
-const api = require('./routes/index');
+const express = require("express");
+const path = require("path");
+const api = require("./routes/index");
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Middleware to serve up static assets from the public folder
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // Send all the requests that begin with /api to the index.js in the routes folder
-app.use('/api', api);
+app.use("/api", api);
 
 // This view route is a GET route for the homepage
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
 );
 
 // This view route is a GET route for the feedback page
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
-
-
-
 
 app.listen(PORT, () =>
   console.log(`Listening for requests at  http://localhost:${PORT} 🚀`)
